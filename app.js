0706cf4fb4281528663bc4d654d0b56f859f1903
“Imen” <imenodoo@gmail.com>
2023-02-08 12:44:56 +0100

    react project

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const port = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send("hello from simple server :)");
-});
 
 app.get("/hello", (req, res) => {
   res.send("hello amouna :)");
 });
+
+//production mode
+//if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname, "./build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "./build/index.html"));
+});
+//}
 
 app.listen(port, () =>
   console.log("> Server is up and running on port : " + port)
